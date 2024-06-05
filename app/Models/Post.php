c57051f0de3634f733801a94f0b08bb8299c94c9
Bruno Juwer <bruno.juwer1997@gmail.com>
2024-06-04 21:39:03 -0300

    Create relationships and accessors/mutators

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -12,4 +15,27 @@ class Post extends Model
     protected $casts = [
         'body' => 'array',
     ];
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'post_id');
+    }
+
+    public function getTittleUpperCaseAttribute(): string // accessor version 8
+    {
+        return strtoupper($this->title);
+    }
+
+    protected function title(): Attribute // accessor and mutator version 10
+    {
+        return Attribute::make(
+            get: fn (string $value) => ucwords($value), // accessor
+            set: fn (string $value) => strtoupper($value), // mutator
+        );
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'post_user', 'post_id', 'user_id');
+    }
 }
