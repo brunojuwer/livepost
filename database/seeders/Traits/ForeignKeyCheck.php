c4d3d7d914b8b069ac1ecfe5102677cf4d720a56
Bruno Juwer <bruno.juwer1997@gmail.com>
2024-06-19 19:20:03 -0300

    change to sqlite

--- database/seeders/Traits/ForeignKeyCheck.php
+++ database/seeders/Traits/ForeignKeyCheck.php
@@ -8,12 +8,14 @@ trait ForeignKeyCheck
 {
   protected function disableForeignKeyCheck()
   {
-    DB::statement('SET FOREIGN_KEY_CHECKS=0');
+    // DB::statement('SET FOREIGN_KEY_CHECKS=0');
+    DB::statement('PRAGMA foreign_keys = OFF');
   }
-
+  
   protected function enableForeignKeyCheck()
   {
-    DB::statement('SET FOREIGN_KEY_CHECKS=1');
+    DB::statement('PRAGMA foreign_keys = ON');
+    // DB::statement('SET FOREIGN_KEY_CHECKS=1');
   }
 
 }
